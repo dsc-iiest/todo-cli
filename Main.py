7f377720bbefbc6e10c607fb0dcf1959c8876c49
Ian Findlay <code@findlayian.com>
2021-10-08 15:37:51 +0100

    Add initial structure of loop exit and adding todo

--- Main.py
+++ Main.py
@@ -6,30 +6,50 @@ import time
 # This is a sample intended application of the to-do cli
 
 if __name__ == "__main__":
-    t = TodoList()
-    # This will be done via interactive CLI (WIP)
-    t.add_todo(
-        task='Wash Dishes',
-        description="Don't forget to use Vim bar also",
-        minute_deadline_offset=1.5)
 
-    t.add_todo(
-        task='Hang Clothes',
-        description="Also install new hanging rope",
-        minute_deadline_offset=0.5)
+    while True:
+        command = input("Enter Command ('exit' to close) : ")
+        if command in ("Exit", "exit"):
+            exit()
 
-    t.add_todo(
-        task='Run Washing Machine',
-        description="Also use Comfort in the end",
-        minute_deadline_offset=1)
+        t= TodoList()
 
-    # multithreading used here st it doesn't interfere with Future to-do Insertion
-    t1 = threading.Thread(target=checker, args=(t,), name='t1')
-    # this makes the thread auto close when the parent thread closes
-    t1.setDaemon(True)
-    t1.start()
+        if command in ("add", "a"):
+            task = input("Enter Title of Todo: ")
+            description = input("Enter Description of Todo: ")
+            try:
+                deadline = float(input("Enter Deadline in minutes "
+                    "(leave blank if not setting one): " ))
+                t.add_todo(task, description, deadline)
+            except ValueError:
+                t.add_todo(task, description)
+            finally:
+                print()
 
-    # WIP: as there should be a Interactive CLI, there will be something
-    # similar to an infinite loop with KeyboardInterrupt/exit() close
-    # here this is simulated as a sleep timer
-    time.sleep(100)
+        t = TodoList()
+        # This will be done via interactive CLI (WIP)
+        t.add_todo(
+            task='Wash Dishes',
+            description="Don't forget to use Vim bar also",
+            minute_deadline_offset=1.5)
+
+        t.add_todo(
+            task='Hang Clothes',
+            description="Also install new hanging rope",
+            minute_deadline_offset=0.5)
+
+        t.add_todo(
+            task='Run Washing Machine',
+            description="Also use Comfort in the end",
+            minute_deadline_offset=1)
+
+        # multithreading used here st it doesn't interfere with Future to-do Insertion
+        t1 = threading.Thread(target=checker, args=(t,), name='t1')
+        # this makes the thread auto close when the parent thread closes
+        t1.setDaemon(True)
+        t1.start()
+
+        # WIP: as there should be a Interactive CLI, there will be something
+        # similar to an infinite loop with KeyboardInterrupt/exit() close
+        # here this is simulated as a sleep timer
+        #time.sleep(100)
