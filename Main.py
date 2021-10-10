7829c82abee6591cb77006b70853b00172e2c6fa
Ian Findlay <code@findlayian.com>
2021-10-10 17:53:46 +0100

    Convert command logic to use argparse

--- Main.py
+++ Main.py
@@ -1,42 +1,62 @@
 from modules.TodoList import TodoList
 from modules.TodoListChecker import checker
+
+import argparse
 import threading
+import shlex
 
 # This is a sample intended application of the to-do cli
+def parse_command(command):
+    """."""
+    parser = argparse.ArgumentParser(description='')
+    subparsers = parser.add_subparsers(dest='command',
+            help='Available to-do list commands')
+
+    parser_add = subparsers.add_parser('add',
+            help='Add a new task to the to-do list')
+    parser_add.add_argument('title', type=str, help='The title of the task')
+    parser_add.add_argument('description', type=str,
+            help='The description of the task')
+    parser_add.add_argument('deadline', type=float, nargs='?',
+            help='An optional deadline argument in minutes')
+
+    parser_add = subparsers.add_parser('view',
+            help='View the to-do list')
+    parser_add.add_argument('view', type=str, default='all', nargs='?',
+            help='The title of the task')
+
+    return parser.parse_args(command)
+
 
 if __name__ == "__main__":
 
     while True:
-        command = input("Enter Command ('help' to list all commands, 'exit' "
-                "to close) : ").lower()
+        command = input("\nEnter Command ('--help' for usage instructions "
+                "commands or 'exit' to close) : ").lower()
 
         if command == "exit":
             exit()
 
-        if command == "help":
-            print("\nAvailable Commands:", end='\n\n')
-            print("'add' - prompts for information about the task and adds it "
-                "to the to-do list")
-            print("'view' - displays all to-do list items")
-            print("'help' - displays this dialogue", end='\n\n')
-            exit()
+        try:
+            parsed = parse_command(shlex.split(command))
+        except SystemExit:
+            continue
 
         t= TodoList()
 
-        if command in ("add", "a"):
-            task = input("Enter Title of Todo: ")
-            description = input("Enter Description of Todo: ")
-            try:
-                deadline = float(input("Enter Deadline in minutes "
-                    "(leave blank if not setting one): " ))
-                t.add_todo(task, description, deadline)
-            except ValueError:
-                t.add_todo(task, description)
-            finally:
-                print()
+        if parsed.command == 'add':
+            t.add_todo(parsed.title, parsed.description, parsed.deadline)
+            print("\nTask added\n")
 
-        t = TodoList()
-        # This will be done via interactive CLI (WIP)
+        elif parsed.command == 'view':
+            # View command logic will go here with
+            # parsed.view will be the keyword for options like 'all', 'overdue'
+            print("To-do list will be printed to screen here")
+            pass
+
+        else:
+            exit()
+
         t.add_todo(
             task='Wash Dishes',
             description="Don't forget to use Vim bar also",
